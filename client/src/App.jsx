8941dad026a72129dfb6ba403d19a67ba391ab4f
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-19 11:07:57 -0500

    Client new

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes/Routes';
+import Layouts from './layouts/Layout';
+
+const App = () => (
+  <Router>
+    <Layouts>
+      <Routes />
+    </Layouts>
+  </Router>
+);
+export default App;
---
+++ client/src/containers/MisPalabras.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MisPalabras = () => {
+  return (
+    <>
+      <h3>Mis Palabras</h3>
+    </>
+  );
+};
+
+export default MisPalabras;
