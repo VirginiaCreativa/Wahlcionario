4ab5fdbdf73fa3f927d4ef812f80bdf8248fc635
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-23 17:01:48 -0500

    Implementa landign part 6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,48 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+/* eslint-disable no-useless-constructor */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-deprecated */
+import React, { Component } from 'react';
+import { BrowserRouter as Router, useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators, compose } from 'redux';
+import { history } from './redux/store/store';
 import { LoadUser } from './redux/actions/Auth.Action';
 import Routes from './routes/Routes';
 import Layouts from './layouts/Layout';
 
-const App = () => {
-  const dispatch = useDispatch();
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  useEffect(() => {
+  componentWillMount() {
     if (localStorage.user) {
-      dispatch(LoadUser());
+      this.props.LoadUser();
+    } else {
+      history.push('landing');
     }
-  }, [dispatch]);
-  return (
-    <Router>
-      <Layouts>
-        <Routes />
-      </Layouts>
-    </Router>
-  );
-};
-export default App;
+  }
+
+  componentWillUpdate() {
+    if (localStorage.user) {
+      this.props.LoadUser();
+    } else {
+      history.push('landing');
+    }
+  }
+
+  render() {
+    return (
+      <Router history={history}>
+        <Layouts>
+          <Routes />
+        </Layouts>
+      </Router>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ LoadUser }, dispatch);
+
+export default compose(connect(null, mapDispatchToProps))(App);
