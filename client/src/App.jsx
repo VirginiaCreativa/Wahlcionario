beff8491bb2bfacdefa0fbad3fa880da79868b36
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-25 09:15:50 -0500

    Solucion package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,31 @@
-/* eslint-disable no-useless-constructor */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/no-deprecated */
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, useHistory } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { bindActionCreators, compose } from 'redux';
 import { history } from './redux/store/store';
 import { LoadUser } from './redux/actions/Auth.Action';
 import Routes from './routes/Routes';
 import Layouts from './layouts/Layout';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+const App = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-  componentWillMount() {
+  useEffect(() => {
     if (localStorage.user) {
-      this.props.LoadUser();
+      useDispatch(LoadUser());
     } else {
       history.push('landing');
     }
-  }
+  });
 
-  componentWillUpdate() {
-    if (localStorage.user) {
-      this.props.LoadUser();
-    } else {
-      history.push('landing');
-    }
-  }
+  return (
+    <Router history={history}>
+      <Layouts>
+        <Routes />
+      </Layouts>
+    </Router>
+  );
+};
 
-  render() {
-    return (
-      <Router history={history}>
-        <Layouts>
-          <Routes />
-        </Layouts>
-      </Router>
-    );
-  }
-}
-
-const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ LoadUser }, dispatch);
-
-export default compose(connect(null, mapDispatchToProps))(App);
+export default App;
