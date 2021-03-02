18a1356fcacb59b97e41b47862c720f251948e66
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-03-02 14:50:16 -0500

    Add redux new Search part 2

--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import auth from './Auth.Reducer';
+import search from './Search.Reducer';
 
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
     auth,
+    search,
   });
 
 export default createRootReducer;
