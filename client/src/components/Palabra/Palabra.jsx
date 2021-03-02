7f11319c846b8b7ab471cca9f6da3125e702307c
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-03-02 18:23:38 -0500

    Implamente Search part 5

--- client/src/components/Palabra/Palabra.jsx
+++ client/src/components/Palabra/Palabra.jsx
@@ -9,7 +9,11 @@ const Palabra = ({ palabra }) => {
         const response = await axios
           .get(`https://api.dictionaryapi.dev/api/v2/entries/es/${palabra}`, {
             headers: {
+              'Content-type': 'application/json; charset=utf-8',
               'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'GET',
+              'cache-control': 'no-cache',
+              'Access-Control-Allow-Headers': 'Content-Type',
             },
           })
           .then((res) => console.log(res));
